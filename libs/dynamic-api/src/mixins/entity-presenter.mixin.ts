9125e77e609e49900e2c380b507e648e435a7e49
Mickael N <mikeonline75@gmail.com>
2024-03-02 17:07:35 +0100

    chore: add the possibility to exclude keys in presenter

--- libs/dynamic-api/src/mixins/entity-presenter.mixin.ts
+++ libs/dynamic-api/src/mixins/entity-presenter.mixin.ts
@@ -1,8 +1,16 @@
 import { Type } from '@nestjs/common';
+import { OmitType } from '@nestjs/swagger';
 import { BaseEntity } from '../models';
 
-function EntityPresenterMixin<Entity extends BaseEntity>(entity: Type<Entity>) {
-  class EntityPresenter extends entity {}
+function EntityPresenterMixin<Entity extends BaseEntity>(
+  entity: Type<Entity>,
+  keysToExclude?: (keyof Entity)[],
+) {
+
+  // @ts-ignore
+  class EntityPresenter extends (
+    keysToExclude ? OmitType(entity, keysToExclude) : entity
+  ) {}
 
   return EntityPresenter;
 }
