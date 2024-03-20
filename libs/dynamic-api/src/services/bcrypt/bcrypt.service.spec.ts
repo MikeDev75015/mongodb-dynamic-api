d0df38778634f81a96b1ca2d0a8fa463d324d656
Mickael N <mikeonline75@gmail.com>
2024-03-20 04:56:31 +0100

    test: add missing bcrypt unit tests

--- libs/dynamic-api/src/services/bcrypt/bcrypt.service.spec.ts
+++ libs/dynamic-api/src/services/bcrypt/bcrypt.service.spec.ts
@@ -1,18 +1,49 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { BcryptService } from './bcrypt.service';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(),
+  compare: jest.fn(),
+}));
 
 describe('BcryptService', () => {
   let service: BcryptService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BcryptService],
-    }).compile();
-
-    service = module.get<BcryptService>(BcryptService);
+    service = new BcryptService();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should have saltOrRounds defined and greater than 0', () => {
+    expect(service['saltOrRounds']).toBeGreaterThan(0);
+  });
+
+  describe('hashPassword', () => {
+    it('should return a hashed password', async () => {
+      const password = 'password';
+      const hash = 'hashedPassword';
+      (bcrypt.hash as jest.Mock).mockResolvedValue(hash);
+      const result = await service.hashPassword(password);
+
+      expect(result).toBe(hash);
+    });
+  });
+
+  describe('comparePassword', () => {
+    const password = 'password';
+    const hash = 'hashedPassword';
+
+    it('should return true if the password matches the hash', async () => {
+      (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+      const result = await service.comparePassword(password, hash);
+
+      expect(result).toBe(true);
+    });
+
+    it('should return false if the password does not match the hash', async () => {
+      (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+      const result = await service.comparePassword(password, hash);
+
+      expect(result).toBe(false);
+    });
   });
 });
