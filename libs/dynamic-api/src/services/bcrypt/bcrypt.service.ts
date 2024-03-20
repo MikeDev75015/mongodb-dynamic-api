f713c3d1f2ee4182ed038ee800b66e3de68034da
Mickael N <mikeonline75@gmail.com>
2024-03-20 04:55:59 +0100

    refactor: rename bcrypt service method names

--- libs/dynamic-api/src/services/bcrypt/bcrypt.service.ts
+++ libs/dynamic-api/src/services/bcrypt/bcrypt.service.ts
@@ -5,11 +5,11 @@ import * as bcrypt from 'bcrypt';
 export class BcryptService {
   private readonly saltOrRounds = 10;
 
-  async hash(password: string): Promise<string> {
+  async hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, this.saltOrRounds);
   }
 
-  async compare(password: string, hash: string): Promise<boolean> {
+  async comparePassword(password: string, hash: string): Promise<boolean> {
     return bcrypt.compare(password, hash);
   }
 }
