c5d1e3070af3d0c228bd79c7343b6ae0a39d3e0e
Mickael N <mikeonline75@gmail.com>
2024-05-25 12:06:34 +0200

    test(e2e): add cases when login options are provided

--- libs/dynamic-api/test/dynamic-api-for-root.e2e-spec.ts
+++ libs/dynamic-api/test/dynamic-api-for-root.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Prop, Schema } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
@@ -588,6 +588,123 @@ describe('DynamicApiModule forRoot (e2e)', () => {
 
         expect(status).toBe(200);
         expect(body).toHaveProperty('isVerified', true);
+      });
+    });
+  });
+
+  describe('POST /login with login options', () => {
+    @Schema({ collection: 'users' })
+    class User extends BaseEntity {
+      @Prop({ type: String, required: true })
+      username: string;
+
+      @Prop({ type: String, required: true })
+      pass: string;
+
+      @Prop({ type: String, default: 'user' })
+      role: 'admin' | 'user' | 'client' = 'user';
+
+      @Prop({ type: Boolean, default: false })
+      isVerified: boolean;
+    }
+
+    const admin = { username: 'admin', pass: 'admin', role: 'admin', isVerified: true };
+    const user = { username: 'user', pass: 'user' };
+    const client = { username: 'client', pass: 'client', role: 'client', isVerified: true };
+
+    beforeEach(async () => {
+      const bcryptService = new BcryptService();
+
+      const fixtures = async (_: Connection) => {
+        const model = await getModelFromEntity(User);
+        await model.insertMany([
+          { ...admin, pass: await bcryptService.hashPassword(admin.pass) },
+          { ...user, pass: await bcryptService.hashPassword(user.pass) },
+          { ...client, pass: await bcryptService.hashPassword(client.pass) },
+        ]);
+      };
+
+      await initModule({
+        useAuth: {
+          userEntity: User,
+          login: {
+            loginField: 'username',
+            passwordField: 'pass',
+            additionalFields: ['role', 'isVerified'],
+            abilityPredicate: (user: User) => user.role === 'admin' || user.role === 'user',
+            callback: async (user: User) => {
+              if (user.isVerified) {
+                return;
+              }
+
+              throw new UnauthorizedException(`Hello ${user.username}, you must verify your account first!`);
+            },
+          },
+        },
+      }, fixtures);
+    });
+
+    describe('loginField', () => {
+      it('should throw an unauthorized exception if loginField is missing', async () => {
+        const { body, status } = await server.post('/auth/login', { pass: 'test' });
+
+        expect(status).toBe(401);
+        expect(body).toEqual({
+          message: 'Unauthorized',
+          statusCode: 401,
+        });
+      });
+    });
+
+    describe('passwordField', () => {
+      it('should throw an unauthorized exception if passwordField is missing', async () => {
+        const { body, status } = await server.post('/auth/login', { username: 'unit' });
+
+        expect(status).toBe(401);
+        expect(body).toEqual({
+          message: 'Unauthorized',
+          statusCode: 401,
+        });
+      });
+    });
+
+    describe('abilityPredicate', () => {
+      it('should throw an forbidden exception if user role is not admin or user', async () => {
+        const { username, pass } = client;
+        const { body, status } = await server.post('/auth/login', { username, pass });
+
+        expect(status).toBe(403);
+        expect(body).toEqual({
+          error: 'Forbidden',
+          message: 'Access denied',
+          statusCode: 403,
+        });
+      });
+    });
+
+    describe('callback', () => {
+      it('should throw an unauthorized exception if user is not verified', async () => {
+        const { username, pass } = user;
+        const { body, status } = await server.post('/auth/login', { username, pass });
+
+        expect(status).toBe(401);
+        expect(body).toEqual({
+          error: 'Unauthorized',
+          message: `Hello ${username}, you must verify your account first!`,
+          statusCode: 401,
+        });
+      });
+    });
+
+    describe('additionalFields', () => {
+      it('should return additional fields', async () => {
+        const { username, pass } = admin;
+        const { body: { accessToken } } = await server.post('/auth/login', { username, pass });
+
+        const { body, status } = await server.get('/auth/account', { headers: { Authorization: `Bearer ${accessToken}` } });
+
+        expect(status).toBe(200);
+        expect(body).toEqual({ id: expect.any(String), username: 'admin', role: 'admin', isVerified: true });
       });
     });
   });
