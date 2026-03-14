f8a4bca30b9d422407c4c7fbec22df32caeb9725
Mickael N. <mikeonline75@gmail.com>
2026-03-14 01:45:16 +0100

    feat(services): add DynamicApiBroadcastService for HTTP broadcast support

--- libs/dynamic-api/src/services/index.ts
+++ libs/dynamic-api/src/services/index.ts
@@ -1,3 +1,4 @@
 export * from './base/base.service';
 export * from './bcrypt/bcrypt.service';
+export * from './dynamic-api-broadcast/dynamic-api-broadcast.service';
 export * from './dynamic-api-global-state/dynamic-api-global-state.service';
