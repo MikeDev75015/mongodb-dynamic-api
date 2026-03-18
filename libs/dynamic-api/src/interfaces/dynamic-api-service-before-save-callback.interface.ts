aa6467f839a753770e40ebeaf0c742b11b67e3da
Mickael N. <mikeonline75@gmail.com>
2026-03-18 18:19:49 +0100

    feat(interfaces): add short aliases for callback types and deprecate verbose names - Define BeforeSaveCreateContext, BeforeSaveCreateManyContext, BeforeSaveUpdateContext,   BeforeSaveUpdateManyContext, BeforeSaveReplaceContext, BeforeSaveDeleteContext,   BeforeSaveDeleteManyContext, BeforeSaveDuplicateContext, BeforeSaveDuplicateManyContext   as primary types; deprecate DynamicApiServiceBeforeSave* long names - Define BeforeSaveCallback, BeforeSaveListCallback, BeforeSaveDeleteCallback   as primary types; deprecate DynamicApiServiceBeforeSave*Callback long names - Define AfterSaveCallback as primary type; deprecate DynamicApiServiceCallback - Define CallbackMethods as primary type; deprecate DynamicApiCallbackMethods - All deprecated aliases remain exported for backward compatibility

--- libs/dynamic-api/src/interfaces/dynamic-api-service-before-save-callback.interface.ts
+++ libs/dynamic-api/src/interfaces/dynamic-api-service-before-save-callback.interface.ts
@@ -1,23 +1,114 @@
 import { BaseEntity } from '../models';
-import { DynamicApiCallbackMethods } from './dynamic-api-service-callback.interface';
+import { CallbackMethods } from './dynamic-api-service-callback.interface';
 
-type DynamicApiServiceBeforeSaveCreateContext<Entity extends BaseEntity> = {
+type BeforeSaveCreateContext<Entity extends BaseEntity> = {
   toCreate: Partial<Entity>;
 }
 
-type DynamicApiServiceBeforeSaveUpdateContext<Entity extends BaseEntity> = {
+type BeforeSaveCreateManyContext<Entity extends BaseEntity> = {
+  toCreate: Partial<Entity>[];
+}
+
+type BeforeSaveUpdateContext<Entity extends BaseEntity> = {
   id: string;
   update: Partial<Entity>;
 }
 
-type DynamicApiServiceBeforeSaveCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = (
+type BeforeSaveUpdateManyContext<Entity extends BaseEntity> = {
+  ids: string[];
+  update: Partial<Entity>;
+}
+
+type BeforeSaveReplaceContext<Entity extends BaseEntity> = {
+  id: string;
+  replacement: Partial<Entity>;
+}
+
+type BeforeSaveDeleteContext = {
+  id: string;
+}
+
+type BeforeSaveDeleteManyContext = {
+  ids: string[];
+}
+
+type BeforeSaveDuplicateContext<Entity extends BaseEntity> = {
+  id: string;
+  override?: Partial<Entity>;
+}
+
+type BeforeSaveDuplicateManyContext<Entity extends BaseEntity> = {
+  ids: string[];
+  override?: Partial<Entity>;
+}
+
+type BeforeSaveCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = (
   entity: Entity | undefined,
   context: Context,
-  methods: DynamicApiCallbackMethods,
+  methods: CallbackMethods,
 ) => Promise<Partial<Entity>>;
 
+type BeforeSaveListCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = (
+  entity: Entity | undefined,
+  context: Context,
+  methods: CallbackMethods,
+) => Promise<Partial<Entity>[]>;
+
+type BeforeSaveDeleteCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = (
+  entity: Entity | undefined,
+  context: Context,
+  methods: CallbackMethods,
+) => Promise<void>;
+
+// --- Deprecated aliases ---
+/** @deprecated Use `BeforeSaveCreateContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveCreateContext<Entity extends BaseEntity> = BeforeSaveCreateContext<Entity>;
+/** @deprecated Use `BeforeSaveCreateManyContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveCreateManyContext<Entity extends BaseEntity> = BeforeSaveCreateManyContext<Entity>;
+/** @deprecated Use `BeforeSaveUpdateContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveUpdateContext<Entity extends BaseEntity> = BeforeSaveUpdateContext<Entity>;
+/** @deprecated Use `BeforeSaveUpdateManyContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveUpdateManyContext<Entity extends BaseEntity> = BeforeSaveUpdateManyContext<Entity>;
+/** @deprecated Use `BeforeSaveReplaceContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveReplaceContext<Entity extends BaseEntity> = BeforeSaveReplaceContext<Entity>;
+/** @deprecated Use `BeforeSaveDeleteContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveDeleteContext = BeforeSaveDeleteContext;
+/** @deprecated Use `BeforeSaveDeleteManyContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveDeleteManyContext = BeforeSaveDeleteManyContext;
+/** @deprecated Use `BeforeSaveDuplicateContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveDuplicateContext<Entity extends BaseEntity> = BeforeSaveDuplicateContext<Entity>;
+/** @deprecated Use `BeforeSaveDuplicateManyContext` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveDuplicateManyContext<Entity extends BaseEntity> = BeforeSaveDuplicateManyContext<Entity>;
+/** @deprecated Use `BeforeSaveCallback` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = BeforeSaveCallback<Entity, Context>;
+/** @deprecated Use `BeforeSaveListCallback` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveListCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = BeforeSaveListCallback<Entity, Context>;
+/** @deprecated Use `BeforeSaveDeleteCallback` instead. Will be removed in v5. */
+type DynamicApiServiceBeforeSaveDeleteCallback<Entity extends BaseEntity, Context = Record<string, unknown>> = BeforeSaveDeleteCallback<Entity, Context>;
+
 export type {
+  BeforeSaveCallback,
+  BeforeSaveListCallback,
+  BeforeSaveDeleteCallback,
+  BeforeSaveCreateContext,
+  BeforeSaveCreateManyContext,
+  BeforeSaveUpdateContext,
+  BeforeSaveUpdateManyContext,
+  BeforeSaveReplaceContext,
+  BeforeSaveDeleteContext,
+  BeforeSaveDeleteManyContext,
+  BeforeSaveDuplicateContext,
+  BeforeSaveDuplicateManyContext,
   DynamicApiServiceBeforeSaveCallback,
+  DynamicApiServiceBeforeSaveListCallback,
+  DynamicApiServiceBeforeSaveDeleteCallback,
   DynamicApiServiceBeforeSaveCreateContext,
+  DynamicApiServiceBeforeSaveCreateManyContext,
   DynamicApiServiceBeforeSaveUpdateContext,
+  DynamicApiServiceBeforeSaveUpdateManyContext,
+  DynamicApiServiceBeforeSaveReplaceContext,
+  DynamicApiServiceBeforeSaveDeleteContext,
+  DynamicApiServiceBeforeSaveDeleteManyContext,
+  DynamicApiServiceBeforeSaveDuplicateContext,
+  DynamicApiServiceBeforeSaveDuplicateManyContext,
 };
